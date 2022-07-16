1403acc34b933df667ed752ad91c494ceb250062
Edwinop <edlucky394@gmail.com>
2022-07-16 10:16:11 -0400

    change nav color

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,11 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navbar from "./components/Navbar.js";
+import Navigation from "./components/Navbar.js";
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Navigation />
     </div>
   );
 }
--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,70 +1,27 @@
-import React from "react";
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Nav,  Navbar} from 'react-bootstrap';
+import './Navbar.css'
 
-const Navbar = () => {
-  return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="#">
-        Navbar
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+const navcolor ={backgroundColor: '#EEECE1'};
+class Navigation extends Component{
 
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">
-              Home <span className="sr-only">(current)</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Features
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Pricing
-            </a>
-          </li>
-          <li className="nav-item dropdown">
-            <a
-              className="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdownMenuLink"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Dropdown link
-            </a>
-            <div
-              className="dropdown-menu"
-              aria-labelledby="navbarDropdownMenuLink"
-            >
-              <a className="dropdown-item" href="#">
-                Action
-              </a>
-              <a className="dropdown-item" href="#">
-                Another action
-              </a>
-              <a className="dropdown-item" href="#">
-                Something else here
-              </a>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
-  );
-};
+    render() {
+        return (
+            <Navbar collapseOnSelect expand="lg" style={navcolor}>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ms-auto">
+                        <Nav.Link href="#features">About</Nav.Link>
+                        <Nav.Link href="#pricing">Projects</Nav.Link>
+                        <Nav.Link href="#deets">Contact</Nav.Link>
+                        <Nav.Link href="#deets">LinkedIn</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
 
-export default Navbar;
+}
+export default Navigation;
