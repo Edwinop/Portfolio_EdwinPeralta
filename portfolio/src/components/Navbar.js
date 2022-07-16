2d16fa18f00974f75e7abcc48936edc04e0d5168
Edwinop <edlucky394@gmail.com>
2022-07-16 11:44:57 -0400

    nav finished

--- portfolio/src/components/Navbar.js
+++ portfolio/src/components/Navbar.js
@@ -1,22 +1,29 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Nav,  Navbar} from 'react-bootstrap';
-import './Navbar.css'
 
-const navcolor ={backgroundColor: '#EEECE1'};
+const navColor ={backgroundColor: '#EEECE1'};
+const navItem = {
+    color : 'black',
+    marginTop: '18px',
+    marginLeft: '50px',
+    marginRight: '50px',
+    fontFamily: 'Quicksand',
+    fontSize: 18
+};
 class Navigation extends Component{
 
     render() {
         return (
-            <Navbar collapseOnSelect expand="lg" style={navcolor}>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar collapseOnSelect expand="lg" style={navColor}>
+                <Navbar.Brand href="#home"></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
-                    <Nav className="ms-auto">
-                        <Nav.Link href="#features">About</Nav.Link>
-                        <Nav.Link href="#pricing">Projects</Nav.Link>
-                        <Nav.Link href="#deets">Contact</Nav.Link>
-                        <Nav.Link href="#deets">LinkedIn</Nav.Link>
+                    <Nav className="ms-auto" >
+                        <Nav.Link href="#features" style={navItem}>ABOUT</Nav.Link>
+                        <Nav.Link href="#pricing"style={navItem}>PROJECTS</Nav.Link>
+                        <Nav.Link href="#deets"style={navItem}>CONTACT</Nav.Link>
+                        <Nav.Link href="#deets"style={navItem}>LINKEDIN</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
