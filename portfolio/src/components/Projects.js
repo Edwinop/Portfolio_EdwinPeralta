d4f920c733fd90418dfabf49cbe773fa9cc60772
Edwinop <edlucky394@gmail.com>
2022-07-17 21:41:07 -0400

    changed pic

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -5,7 +5,7 @@ import { CardActionArea } from '@mui/material';
 import './Border.css'
 import logo from './img/medicinalplant.jpg'
 import logo1 from './img/wordleclone.jpg'
-import logo2 from './img/unrealengine.jpg'
+import logo2 from './img/isaacjones.jpg'
 
 const Projects = () => {  
   return (
@@ -21,9 +21,9 @@ const Projects = () => {
     <MDBCard className="border_color" style={{backgroundColor: '#EEECE1'}}>
     <MDBRow className='row-cols-1 row-cols-md-3 g-4' style={{marginLeft: 280, marginTop: 100, marginBottom: 100}}>
       <MDBCol >
-      <Tilt className="Tilt" options={{ max : 20 }} style={{ height: 325, width: 250 }}>
+      <Tilt id="card" className="Tilt" options={{ max : 20 }} style={{ height: 325, width: 250 }}>
         <CardActionArea>
-        <MDBCard className='h-100' style={{ maxWidth: '15rem',minHeight: '15rem', backgroundColor: '#D9D3B6',boxShadow: '1px 2px 9px'}} >
+        <MDBCard className='h-100 border_color' style={{ maxWidth: '15rem',minHeight: '15rem', backgroundColor: '#D9D3B6'}} >
           <MDBCardImage
             src={logo}
             alt='...'
@@ -43,7 +43,7 @@ const Projects = () => {
       <MDBCol>
       <Tilt className="Tilt" options={{ max : 20 }} style={{ height: 325, width: 250 }}>
       <CardActionArea>
-        <MDBCard className='h-100' style={{ maxWidth: '15rem',minHeight: '15rem',backgroundColor: '#D9D3B6',boxShadow: '1px 2px 9px' }}>
+        <MDBCard className='h-100 border_color' style={{ maxWidth: '15rem',minHeight: '15rem',backgroundColor: '#D9D3B6'}}>
           <MDBCardImage
             src={logo1}
             alt='...'
@@ -62,7 +62,7 @@ const Projects = () => {
       <MDBCol>
       <Tilt className="Tilt" options={{ max : 20 }} style={{ height: 325, width: 250 }}>
       <CardActionArea>
-        <MDBCard className='h-100' style={{ maxWidth: '15rem',minHeight: '15rem',backgroundColor: '#D9D3B6',boxShadow: '1px 2px 9px'}}>
+        <MDBCard className='h-100 border_color' style={{ maxWidth: '15rem',minHeight: '15rem',backgroundColor: '#D9D3B6'}}>
           <MDBCardImage
             src={logo2}
             alt='...'
