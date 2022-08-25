9c8fa01a2f051a64fdfeabfbca74eea4b3e133f6
Edwinop <edlucky394@gmail.com>
2022-08-24 20:21:41 -0400

    commit

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -26,11 +26,7 @@ import React from 'react';
             <div style={heroCardTitle}>Currently looking for an Entry Level <br></br>Front-End Developer Position in NY</div>
             <div style={heroCardName}>Front-End</div>
             <div style={heroCardText}>
-            Javascript, React, Angular, HTML, CSS, Bootstrap
-            </div>
-            <div style={heroCardName}>Back-End</div>
-            <div style={heroCardText}>
-            Java, NodeJS, Express, PHP, MySQL, MongoDB            
+            Javascript, React, HTML, CSS, Material-Ui,Bootstrap
             </div>
             <div style={heroCardName}>Tools</div>
             <div style={heroCardText}>
